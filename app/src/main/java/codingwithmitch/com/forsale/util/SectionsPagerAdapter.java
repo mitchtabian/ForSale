83afd491522bc64f878008211ee6e9e77db0dd02
Mitch <mitch@tabian.ca>
2017-10-16 11:28:35 -0700

    Collapsing Toolbar Setup

---
+++ app/src/main/java/codingwithmitch/com/forsale/util/SectionsPagerAdapter.java
@@ -0,0 +1,36 @@
+package codingwithmitch.com.forsale.util;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by User on 10/16/2017.
+ */
+
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    private static final String TAG = "SectionsPagerAdapter";
+
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    public void addFragment(Fragment fragment){
+        mFragmentList.add(fragment);
+    }
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+}
