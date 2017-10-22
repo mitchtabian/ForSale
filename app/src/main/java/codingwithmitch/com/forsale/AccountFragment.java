7a8f267a53313c09511afd10df418c8fab3cebf6
Mitch <mitch@tabian.ca>
2017-10-22 10:34:38 -0700

    Signing-out a FirebaseUser

--- app/src/main/java/codingwithmitch/com/forsale/AccountFragment.java
+++ app/src/main/java/codingwithmitch/com/forsale/AccountFragment.java
@@ -25,16 +25,65 @@ public class AccountFragment extends Fragment {
 
     private static final String TAG = "AccountFragment";
 
+    //firebase
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
+    //widgets
+    private Button mSignOut;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_account, container, false);
+        mSignOut = (Button) view.findViewById(R.id.sign_out);
+
+        setupFirebaseListener();
+
+        mSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: attempting to sign out the user.");
+                FirebaseAuth.getInstance().signOut();
+            }
+        });
+
 
         return view;
     }
-    
+
+    private void setupFirebaseListener(){
+        Log.d(TAG, "setupFirebaseListener: setting up the auth state listener.");
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    Log.d(TAG, "onAuthStateChanged: signed_in: " + user.getUid());
+                }else{
+                    Log.d(TAG, "onAuthStateChanged: signed_out");
+                    Toast.makeText(getActivity(), "Signed out", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getActivity(), LoginActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                }
+            }
+        };
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseAuth.getInstance().addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if(mAuthStateListener != null){
+            FirebaseAuth.getInstance().removeAuthStateListener(mAuthStateListener);
+        }
+    }
 }
 
 
