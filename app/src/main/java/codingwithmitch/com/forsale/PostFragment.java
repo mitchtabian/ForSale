18a12b1c638922582cceae14e9dc483261609a96
Mitch <mitch@tabian.ca>
2017-10-22 12:48:09 -0700

    Selecting an Image for Uploading

--- app/src/main/java/codingwithmitch/com/forsale/PostFragment.java
+++ app/src/main/java/codingwithmitch/com/forsale/PostFragment.java
@@ -1,11 +1,20 @@
 package codingwithmitch.com.forsale;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import codingwithmitch.com.forsale.util.UniversalImageLoader;
 
 /**
  * Created by User on 10/22/2017.
@@ -15,11 +24,87 @@ public class PostFragment extends Fragment {
 
     private static final String TAG = "PostFragment";
 
+    //widgets
+    private ImageView mPostImage;
+    private EditText mTitle, mDescription, mPrice, mCountry, mStateProvince, mCity, mContactEmail;
+    private Button mPost;
+    private ProgressBar mProgressBar;
+
+    //vars
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_post, container, false);
+        mPostImage = view.findViewById(R.id.post_image);
+        mTitle = view.findViewById(R.id.input_title);
+        mDescription = view.findViewById(R.id.input_description);
+        mPrice = view.findViewById(R.id.input_price);
+        mCountry = view.findViewById(R.id.input_country);
+        mStateProvince = view.findViewById(R.id.input_state_province);
+        mCity = view.findViewById(R.id.input_city);
+        mContactEmail = view.findViewById(R.id.input_email);
+        mPost = view.findViewById(R.id.btn_post);
+        mProgressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+
+        getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
 
         return view;
     }
+
+    private void init(){
+
+        mPostImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: opening dialog to choose new photo");
+
+            }
+        });
+    }
+
+    private void resetFields(){
+        UniversalImageLoader.setImage("", mPostImage);
+        mTitle.setText("");
+        mDescription.setText("");
+        mPrice.setText("");
+        mCountry.setText("");
+        mStateProvince.setText("");
+        mCity.setText("");
+        mContactEmail.setText("");
+    }
+
+    private void showProgressBar(){
+        mProgressBar.setVisibility(View.VISIBLE);
+
+    }
+
+    private void hideProgressBar(){
+        if(mProgressBar.getVisibility() == View.VISIBLE){
+            mProgressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    /**
+     * Return true if the @param is null
+     * @param string
+     * @return
+     */
+    private boolean isEmpty(String string){
+        return string.equals("");
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
