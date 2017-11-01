2f27ca5013e589cef755115e093c66e3cb455705
Mitch <mitch@tabian.ca>
2017-10-31 19:09:08 -0700

    ElasticSearch Query Prep

--- app/src/main/java/codingwithmitch/com/forsale/SearchFragment.java
+++ app/src/main/java/codingwithmitch/com/forsale/SearchFragment.java
@@ -1,7 +1,9 @@
 package codingwithmitch.com.forsale;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -13,6 +15,12 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by User on 10/22/2017.
@@ -26,6 +34,12 @@ public class SearchFragment extends Fragment {
     private ImageView mFilters;
     private EditText mSearchText;
 
+    //vars
+    private String mElasticSearchPassword;
+    private String mPrefCity;
+    private String mPrefStateProv;
+    private String mPrefCountry;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -33,6 +47,7 @@ public class SearchFragment extends Fragment {
         mFilters = (ImageView) view.findViewById(R.id.ic_search);
         mSearchText = (EditText) view.findViewById(R.id.input_search);
 
+        getElasticSearchPassword();
         init();
 
         return view;
@@ -56,13 +71,51 @@ public class SearchFragment extends Fragment {
                         || event.getAction() == KeyEvent.ACTION_DOWN
                         || event.getKeyCode() == KeyEvent.KEYCODE_ENTER){
 
-
+                    
                 }
 
                 return false;
             }
         });
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getFilters();
+    }
+
+    private void getElasticSearchPassword(){
+        Log.d(TAG, "getElasticSearchPassword: retrieving elasticsearch password.");
+
+        Query query = FirebaseDatabase.getInstance().getReference()
+                .child(getString(R.string.node_elasticsearch))
+                .child(getString(R.string.field_password));
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                DataSnapshot singleSnapshot = dataSnapshot.getChildren().iterator().next();
+                mElasticSearchPassword = singleSnapshot.getValue().toString();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void getFilters(){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        mPrefCity = preferences.getString(getString(R.string.preferences_city), "");
+        mPrefStateProv = preferences.getString(getString(R.string.preferences_state_province), "");
+        mPrefCountry = preferences.getString(getString(R.string.preferences_country), "");
+
+        Log.d(TAG, "getFilters: got filters: \ncity: " + mPrefCity + "\nState/Prov: " + mPrefStateProv
+                + "\nCountry: " + mPrefCountry);
+    }
+
 }
 
 
