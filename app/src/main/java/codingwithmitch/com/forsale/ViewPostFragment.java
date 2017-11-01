97058ff52d4e63af3ad6dc59102dcdbd0b5efb70
Mitch <mitch@tabian.ca>
2017-11-01 13:31:10 -0700

    Saving Posts to Watch-List

--- app/src/main/java/codingwithmitch/com/forsale/ViewPostFragment.java
+++ app/src/main/java/codingwithmitch/com/forsale/ViewPostFragment.java
@@ -18,7 +18,9 @@ import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -76,6 +78,52 @@ public class ViewPostFragment extends Fragment {
     private void init(){
         getPostInfo();
 
+        //view the post in more detail
+        Fragment fragment = (Fragment)((SearchActivity)getActivity()).getSupportFragmentManager()
+                .findFragmentByTag("android:switcher:" + R.id.viewpager_container + ":" +
+                        ((SearchActivity)getActivity()).mViewPager.getCurrentItem());
+        if(fragment != null){
+            //SearchFragment (AKA #0)
+            if(fragment.getTag().equals("android:switcher:" + R.id.viewpager_container + ":0")){
+                Log.d(TAG, "onClick: switching to: " + getActivity().getString(R.string.fragment_view_post));
+
+               mSavePost.setOnClickListener(new View.OnClickListener() {
+                   @Override
+                   public void onClick(View v) {
+                       addItemToWatchList();
+                   }
+               });
+
+                mWatchList.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        addItemToWatchList();
+                    }
+                });
+            }
+            //WatchList Fragment (AKA #1)
+            else if(fragment.getTag().equals("android:switcher:" + R.id.viewpager_container + ":1")){
+                Log.d(TAG, "onClick: switching to: " + getActivity().getString(R.string.fragment_watch_list));
+
+                mSavePost.setText("remove post");
+                mSavePost.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        removeItemFromWatchList();
+                        getActivity().getSupportFragmentManager().popBackStack();
+                    }
+                });
+
+                mWatchList.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        removeItemFromWatchList();
+                        getActivity().getSupportFragmentManager().popBackStack();
+                    }
+                });
+            }
+        }
+
         mContactSeller.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -99,6 +147,33 @@ public class ViewPostFragment extends Fragment {
         mWatchList.setColorFilter(Color.BLUE);
         mClose.setImageBitmap(createOutline(BitmapFactory.decodeResource(getResources(), R.drawable.ic_x_white)));
         mClose.setColorFilter(Color.BLUE);
+    }
+
+    private void addItemToWatchList(){
+        Log.d(TAG, "addItemToWatchList: adding item to watch list.");
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+
+        reference.child(getString(R.string.node_watch_list))
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .child(mPostId)
+                .child(getString(R.string.field_post_id))
+                .setValue(mPostId);
+
+        Toast.makeText(getActivity(), "Added to watch list", Toast.LENGTH_SHORT).show();
+    }
+
+    private void removeItemFromWatchList(){
+        Log.d(TAG, "removeItemFromWatchList: removing item from watch list.");
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+
+        reference.child(getString(R.string.node_watch_list))
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .child(mPostId)
+                .removeValue();
+
+        Toast.makeText(getActivity(), "Removed from watch list", Toast.LENGTH_SHORT).show();
     }
 
     private void getPostInfo(){
